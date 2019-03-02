71d10c74a10ab88a94f41aa7218cb03b094827c8
Ricardo <eehaha@gmail.com>
2019-03-02 15:14:59 +0800

    two view controller each link to another

---
+++ MultipleViewControllers/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  MultipleViewControllers
+//
+//  Created by Ricardo Hui on 2/3/2019.
+//  Copyright © 2019 Ricardo Hui. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
